138c21784484c68ef032b3423288f24c7a8ec498
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-04-26 14:03:10 +0100

    merge origin with local

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,11 @@
 var path = require('path'),
-rootPath = path.normalize(__dirname + '/../..');
+  rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
+var dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
+  root: rootPath,
+  port: process.env.PORT || 3000,
+  db: process.env.MONGOHQ_URL
 };
---
+++ public/test/sample.spec.js
@@ -0,0 +1,9 @@
+describe('Filters', () => {
+    beforeEach(module('app'));
+
+    describe('test', () => {
+        it('test', () => {
+            expect(true).toBe(true);
+        });
+    })
+});
