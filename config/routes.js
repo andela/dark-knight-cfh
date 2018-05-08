2d41eae4f5fdeb05748f1873362f93f5fb566cfd
Anaeze Nsoffor <andeladeveloper@Andelas-MacBook-Pro-22.local>
2018-05-08 08:55:58 +0100

    [Feature #156881784] Implement leaderboard
    1. Create route for leader board
    2. Implement method to get users sorted by points in descending order
    
    [Finishes #156881784]

--- config/routes.js
+++ config/routes.js
@@ -46,14 +46,22 @@ module.exports = (app, passport) => {
   app.get('/users/:userId', users.show);
 
   // Setting the facebook oauth routes
-  app.get('/auth/facebook', passport.authenticate('facebook', {
-    scope: ['public_profile', 'email'],
-    failureRedirect: '/signin'
-  }), users.signin);
+  app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook', {
+      scope: ['public_profile', 'email'],
+      failureRedirect: '/signin'
+    }),
+    users.signin
+  );
 
-  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-    failureRedirect: '/signin'
-  }), users.authCallback);
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      failureRedirect: '/signin'
+    }),
+    users.authCallback
+  );
 
   // Setting the twitter oauth routes
   app.get(
@@ -73,18 +81,22 @@ module.exports = (app, passport) => {
   );
 
   // Setting the google oauth routes
-  app.get('/auth/google', passport.authenticate('google', {
-    failureRedirect: '/signin',
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email'
-    ]
-  }), users.signin);
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      failureRedirect: '/signin',
+      scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+    }),
+    users.signin
+  );
 
-
-  app.get('/auth/google/callback', passport.authenticate('google', {
-    failureRedirect: '/signin'
-  }), users.authCallback);
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+      failureRedirect: '/signin'
+    }),
+    users.authCallback
+  );
 
   // Finish with setting up the userId param
   app.param('userId', users.user);
@@ -113,4 +125,7 @@ module.exports = (app, passport) => {
 
   app.post('/api/games/:id/start', index.start);
   app.get('/api/profile', verifyJWT, users.profile);
+
+  // Leaderboard Route
+  app.get('/api/leaderboard', verifyJWT, users.leaderboard);
 };
